66d464f2ea23e9b6b3a77aad58b9f1ebd7dbb1d6
Pedro Meseguer <meseguer.p@pg.com>
2021-03-14 11:15:31 -0600

    Adding React Application

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is my App!! and here is a link...maybe!
+        </a>
+        <a
+        >
+          My name is Pedro and here there is no link!
+        </a>
+        <p>
+          Testing 123
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
